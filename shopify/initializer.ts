824feb9e16a7eb86b5d97ed06076192d94260e99
tsurutan <tsurutan.android@gmail.com>
2022-09-25 15:36:12 +0900

    feat: 🎸 Use mongoDB

--- shopify/initializer.ts
+++ shopify/initializer.ts
@@ -2,7 +2,7 @@ import Shopify, {ApiVersion} from "@shopify/shopify-api";
 import ShopifySession from "@shopify/shopify-api/dist/auth/session";
 
 export const setupShopify = () => {
-  const {API_KEY, API_SECRET_KEY, SCOPES, HOST, HOST_SCHEME, DB_PASSWORD } = process.env;
+  const {API_KEY, API_SECRET_KEY, SCOPES, HOST, HOST_SCHEME, DB_URL, DB_NAME } = process.env;
 
   Shopify.Context.initialize({
     API_KEY: API_KEY!,
@@ -12,8 +12,6 @@ export const setupShopify = () => {
     HOST_SCHEME,
     IS_EMBEDDED_APP: false,
     API_VERSION: ApiVersion.April22,
-    SESSION_STORAGE: new ShopifySession.RedisSessionStorage(new URL('https://apn1-fit-wasp-33838.upstash.io'), {
-      password: DB_PASSWORD
-    })
+    SESSION_STORAGE: new ShopifySession.MongoDBSessionStorage(new URL(DB_URL!), DB_NAME!)
   });
 }
