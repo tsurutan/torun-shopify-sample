7a0e87e462e135110e991abaf49d4ac087a106ef
tsurutan <tsurutan.android@gmail.com>
2022-09-25 15:42:04 +0900

    feat: 🎸 Show error message

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -8,9 +8,12 @@ export default async (
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) => {
-  const session = await Shopify.Auth.validateAuthCallback(req, res, req.query as unknown as AuthQuery);
-
-  console.log(session.accessToken);
-  return res.redirect(`/?host=${req.query.host}&shop=${req.query.shop}`);
+  try {
+    const session = await Shopify.Auth.validateAuthCallback(req, res, req.query as unknown as AuthQuery);
+    console.log(session.accessToken);
+    return res.redirect(`/?host=${req.query.host}&shop=${req.query.shop}`);
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message})
+  }
 }
 
