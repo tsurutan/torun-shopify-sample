6cd8469f89d376803fe6b68976ef4ffdb5294405
tsurutan <tsurutan.android@gmail.com>
2022-09-25 12:40:53 +0900

    feat: 🎸 Explicit session storage

--- shopify/initializer.ts
+++ shopify/initializer.ts
@@ -1,4 +1,5 @@
 import Shopify, {ApiVersion} from "@shopify/shopify-api";
+import ShopifySession from "@shopify/shopify-api/dist/auth/session";
 
 export const setupShopify = () => {
   const {API_KEY, API_SECRET_KEY, SCOPES, SHOP, HOST, HOST_SCHEME} = process.env;
@@ -10,6 +11,7 @@ export const setupShopify = () => {
     HOST_NAME: HOST!,
     HOST_SCHEME,
     IS_EMBEDDED_APP: false,
-    API_VERSION: ApiVersion.April22
+    API_VERSION: ApiVersion.April22,
+    SESSION_STORAGE: new ShopifySession.MemorySessionStorage()
   });
 }
